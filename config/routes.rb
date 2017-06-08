69043df7d62b8e317c24059b76d1c81cf0c7bf9e
Chris Ryan <chrisryan@hotmail.ca>
2017-06-08 00:17:32 -0700

    added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
   get '/', to: 'welcome#index', as: 'home'
-  
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :users, only: [:new, :create]
+
+  root "posts#index"
+
+  resources :sessions, only: [:new, :create]do
+    delete :destroy, on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
