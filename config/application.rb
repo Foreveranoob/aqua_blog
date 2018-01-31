671db82675fc276d0fc58665a72677b888df95a3
Chris Ryan <chrisryan@hotmail.ca>
2018-01-30 22:46:58 -0800

    t

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ require 'carrierwave'
 require 'faker'
 # require "rails/test_unit/railtie"
 
+config.serve_static_assets = true
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,7 +25,6 @@ module AquaBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
